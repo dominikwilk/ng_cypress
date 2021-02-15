8e5cd30a8c620b21428f30478cb034da1b6f5c55
Dominik Wilk <domin.wilk@gmail.com>
2021-02-15 01:28:35 +0100

    fixed typo

--- cypress/integration/imdb/login_spec.js
+++ cypress/integration/imdb/login_spec.js
@@ -23,7 +23,7 @@ describe('login', () => {
         cy.url().should('include', '/registration/signin')
         cy.get('[class="list-group"]').should('be.visible')
     })
-    it('load login page and type login without password', () => {
+    it('load login page and type login without providing password', () => {
         cy.contains('Sign in with IMDb').click()
         cy.get('[id="ap_email"]').should('be.visible')
         cy.url().should('include', '/ap/signin')
